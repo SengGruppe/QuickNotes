b3b8422697c12ac8f42cc4dbeb7914957545629a
Nico Eckes <nicoeckes@gmail.com>
2018-06-06 11:06:59 +0200

    Implemented settings screen with example setting

--- app/src/main/java/io/github/senggruppe/quicknotes/fragments/FragmentSettings.java
+++ app/src/main/java/io/github/senggruppe/quicknotes/fragments/FragmentSettings.java
@@ -1,5 +1,20 @@
 package io.github.senggruppe.quicknotes.fragments;
 
-//TODO implement
-public class FragmentSettings extends android.app.Fragment {
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+import io.github.senggruppe.quicknotes.R;
+
+public class FragmentSettings extends PreferenceFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.settings);
+
+        CheckBoxPreference cb01 = (CheckBoxPreference) findPreference("cb01");
+        cb01.setChecked(true);
+    }
 }
