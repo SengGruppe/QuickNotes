8b79f3dc0883be5a4fa60a00523ce54056736948
Frederic Furtwängler <frederic.r.furtwaengler@th-bingen.de>
2018-06-22 13:01:52 +0200

    PopUp Menü Funktionalitöt hinzugefügt Button Funktionalität geändert

--- app/src/main/java/io/github/senggruppe/quicknotes/activities/PopActivity.java
+++ app/src/main/java/io/github/senggruppe/quicknotes/activities/PopActivity.java
@@ -6,13 +6,19 @@ import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.PopupMenu.OnMenuItemClickListener;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+
+
 
 import com.crashlytics.android.Crashlytics;
 
@@ -28,10 +34,11 @@ import io.github.senggruppe.quicknotes.core.Note;
 import io.github.senggruppe.quicknotes.core.NoteStorage;
 import io.github.senggruppe.quicknotes.core.conditions.TimeCondition;
 import io.github.senggruppe.quicknotes.fragments.DatePickerFragment;
+import io.github.senggruppe.quicknotes.fragments.FragmentNotes;
 import io.github.senggruppe.quicknotes.fragments.TimePickerFragment;
 import io.github.senggruppe.quicknotes.util.Utils;
 
-public class PopActivity extends AppCompatActivity implements Utils.PermissionResultHandler {
+public class PopActivity extends AppCompatActivity implements Utils.PermissionResultHandler,OnMenuItemClickListener {
     Calendar calendar = null;
     private MediaRecorder recorder;
     private File audioMessage;
@@ -63,16 +70,18 @@ public class PopActivity extends AppCompatActivity implements Utils.PermissionRe
         FloatingActionButton btnRecord = findViewById(R.id.btnRecord);
 
         player = findViewById(R.id.activity_pop_player);
-        Button datePickButton = findViewById(R.id.datePickButton);
-        Button timePickButton = findViewById(R.id.timePickButton);
-        datePickButton.setOnClickListener((View View) -> {
+        Button specification_Button = findViewById(R.id.SpecifyButton);
+        Button add_Label_Button = findViewById(R.id.AddLabelButton);
+
+
+        /*datePickButton.setOnClickListener((View View) -> {
             DatePickerFragment newFragment = new DatePickerFragment();
             newFragment.show(getSupportFragmentManager(), "datePicker");
         });
         timePickButton.setOnClickListener(View -> {
             TimePickerFragment newFragment = new TimePickerFragment();
             newFragment.show(getSupportFragmentManager(), "timePicker");
-        });
+        });*/
         saveButton.setOnClickListener(View -> {
             try {
                 Note addedNote = new Note(editNote.getText().toString(), null, audioMessage);
@@ -83,6 +92,8 @@ public class PopActivity extends AppCompatActivity implements Utils.PermissionRe
                 Condition timeCondition = TimeCondition.setupTimedNotification(this, addedNote, calendar);
                 addedNote.conditions.add(timeCondition);
                 NoteStorage.get(this).addNote(this, addedNote);
+                FragmentNotes.notifyDataSetChanged();
+
 
 
             } catch (Exception e) {
@@ -98,6 +109,31 @@ public class PopActivity extends AppCompatActivity implements Utils.PermissionRe
             return true;
         });
     }
+    public void showPopUp(View v){
+        PopupMenu popup = new PopupMenu(this,v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.specification_popup);
+        popup.show();
+    }
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.PopUpDateButton:
+                DatePickerFragment DateFragment = new DatePickerFragment();
+                DateFragment.show(getSupportFragmentManager(), "datePicker");
+                return true;
+
+            case R.id.PopUpTimeButton:
+                TimePickerFragment TimeFragment = new TimePickerFragment();
+                TimeFragment.show(getSupportFragmentManager(), "timePicker");
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+
 
     private void stopRecord() {
         if (recorder != null) {
@@ -156,4 +192,6 @@ public class PopActivity extends AppCompatActivity implements Utils.PermissionRe
         } else
             calendar = c;
     }
+
+
 }
